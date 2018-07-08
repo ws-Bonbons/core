66c4d2a338adea63a3ad38d8050099e4f7959fff
Big Mogician <sunaoran@youzan.com>
2018-07-08 20:37:41 +0800

    add global scope for bonbons

--- dist/src/index.d.ts
+++ dist/src/index.d.ts
@@ -1,3 +1,10 @@
+import * as Metadata from "@bonbons/contracts";
+import * as DISystem from "@bonbons/di";
+import * as DefaultOptions from "@bonbons/options";
+import * as PipeSupport from "@bonbons/pipes";
+import * as ControllerSupport from "@bonbons/controllers";
+import * as DecoratorSupport from "@bonbons/decorators";
+import * as BonbonsPlugins from "@bonbons/plugins";
 import { BonbonsServer, BaseApp } from "./server";
 /**
  * Bonbons
@@ -8,4 +15,6 @@ import { BonbonsServer, BaseApp } from "./server";
  */
 export declare const Bonbons: typeof BonbonsServer;
 export * from "./decorator";
+export declare type BonbonsScopeType = typeof Metadata & typeof BonbonsPlugins & typeof DISystem & typeof DefaultOptions & typeof PipeSupport & typeof ControllerSupport & typeof DecoratorSupport;
+export declare const BonbonsScope: BonbonsScopeType;
 export { BaseApp, BonbonsServer as Server };
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
+import * as Metadata from "@bonbons/contracts";
+import * as DISystem from "@bonbons/di";
+import * as DefaultOptions from "@bonbons/options";
+import * as PipeSupport from "@bonbons/pipes";
+import * as ControllerSupport from "@bonbons/controllers";
+import * as DecoratorSupport from "@bonbons/decorators";
+import * as BonbonsPlugins from "@bonbons/plugins";
+
 import { BonbonsServer, BaseApp } from "./server";
 
 /**
@@ -10,6 +18,23 @@ import { BonbonsServer, BaseApp } from "./server";
 export const Bonbons = BonbonsServer;
 
 export * from "./decorator";
+
+export type BonbonsScopeType =
+  typeof Metadata & typeof BonbonsPlugins &
+  typeof DISystem & typeof DefaultOptions &
+  typeof PipeSupport & typeof ControllerSupport &
+  typeof DecoratorSupport;
+
+export const BonbonsScope: BonbonsScopeType = Object.assign(
+  {},
+  Metadata,
+  BonbonsPlugins,
+  DISystem,
+  DefaultOptions,
+  PipeSupport,
+  ControllerSupport,
+  DecoratorSupport
+);
 
 export {
   BaseApp,
